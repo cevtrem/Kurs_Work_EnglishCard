25a84f4e6d3fc480f672a65aab51a4cb0ba425d6
Mladinsky <cevtrem@gmail.com>
2025-02-17 15:13:47 +0300

    Initial commit

---
+++ connect_with_db.py
@@ -0,0 +1,308 @@
+"""
+Модуль connect_with_db отвечает за взаимодействие с базой данных,
+включая создание подключения, управление таблицами
+и выполнение запросов для получения и обработки данных.
+"""
+
+import configparser
+import json
+import random
+import psycopg2
+
+
+def read_user_data():
+    """
+    Функция для чтения данных пользователя (имя пользователя, пароль и имя базы данных)
+     из конфигурационного файла settings.ini.
+    """
+    config = configparser.ConfigParser()
+    config.read("/home/mladinsky/Study/Kurs_Work_EnglishCard/settings.ini")
+    user_db = config['BD']['user']
+    password_db = config['BD']['password']
+    name_db = config['BD']['db_name']
+    return user_db, password_db, name_db
+
+
+class Words:
+    """
+    Класс Words предназначен для управления словарем слов в базе данных,
+    включая создание таблицы слов, заполнение таблицы данными из JSON-файла,
+    а также получение случайных слов и их переводов для использования в телеграм-боте.
+    """
+
+    def __init__(self, user_db, password_db, name_db):
+        self.user = user_db
+        self.password = password_db
+        self.db_name = name_db
+
+    def connect_to_database(self):
+        """Метод для создания подключения к базе данных"""
+        return psycopg2.connect(database=self.db_name, user=self.user, password=self.password)
+
+    def get_data_from_json(self, json_file_name):
+        """
+        Метод для чтения данных из указанного JSON-файла,
+        который используется для заполнения таблицы слов в базе данных.
+        """
+        with open(f'Data_Base_Words/{json_file_name}', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return data
+
+    def drop_table_words(self, table_name):
+        """Метод для удаления указанной таблицы из базы данных."""
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"""
+                DROP TABLE {table_name};
+                """)
+                conn.commit()
+
+    def create_table_words(self):
+        """
+        Метод для создания таблицы words,
+        если она еще не существует, с полями id, en, ru и tr.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS words (
+                id SERIAL PRIMARY KEY UNIQUE,
+                en VARCHAR(150) UNIQUE,
+                ru VARCHAR(150),
+                tr VARCHAR(150)
+                    );''')
+                conn.commit()
+
+    def fill_table_words(self):
+        """Метод для заполнения таблицы words данными из JSON-файла words.json."""
+        data = self.get_data_from_json('words.json')
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                for line in data:
+                    cursor.execute('''
+                    INSERT INTO words (en, ru, tr) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING''',
+                    (line["en"], line["ru"], line["tr"]))
+                conn.commit()
+
+    def get_word_from_words(self):
+        """
+        Метод для получения случайного слова из таблицы words,
+        его перевода и списка других случайных слов для создания карточек.
+        """
+        others = []
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''SELECT MAX(id) FROM words''')
+                max_id = cursor.fetchone()[0]
+                target_id = random.randint(0, max_id)
+                cursor.execute('''
+                                    SELECT en FROM words WHERE id = %s
+                                     ''', (target_id,))
+                target_word = cursor.fetchone()[0]
+                cursor.execute('''
+                                    SELECT ru FROM words WHERE id = %s
+                                     ''', (target_id,))
+                translate_word = cursor.fetchone()[0]
+                for _ in range(3):
+                    random_other_id = random.randint(0, max_id)
+                    if target_id != random_other_id:
+                        cursor.execute('''
+                                            SELECT en FROM words WHERE id = %s
+                                            ''', (random_other_id,))
+                        other_word = cursor.fetchone()[0]
+                        others.append(other_word)
+        return target_word, translate_word, others, target_id
+
+    def count_words(self):
+        """Метод для получения количества слов в таблице words"""
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT COUNT(*) FROM words;")
+                return cursor.fetchone()[0]
+
+
+class Users:
+    """
+    Класс Users предназначен для управления пользователями в базе данных,
+    включая создание таблицы пользователей,
+    сохранение новых пользователей и получение списка известных пользователей.
+    """
+
+    def __init__(self, user_db, password_db, name_db):
+        self.user = user_db
+        self.password = password_db
+        self.db_name = name_db
+
+    def connect_to_database(self):
+        """Метод для создания подключения к базе данных"""
+        return psycopg2.connect(database=self.db_name, user=self.user, password=self.password)
+
+    def create_table_users(self):
+        """Метод для создания таблицы users,
+        если она еще не существует, с полями id, chat_id и username.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY UNIQUE,
+                chat_id BIGINT UNIQUE,
+                username VARCHAR(100)
+                    );''')
+
+    def save_user(self, name_user, chat_id):
+        """
+        Метод для добавления нового пользователя в таблицу users,
+        передавая имя пользователя и его chat_id.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("INSERT INTO users (username, chat_id) VALUES (%s, %s);",
+                               (name_user, chat_id))
+                conn.commit()
+
+    def get_known_users(self):
+        """
+        Метод для получения всех известных пользователей из таблицы users,
+        возвращая словарь, где ключ - это chat_id, а значение - id пользователя.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                known_users = {}
+                cursor.execute("""
+                        SELECT * FROM users;
+                        """)
+                for data in cursor.fetchall():
+                    user_id = data[0]
+                    chat_id = data[1]
+                    known_users[chat_id] = user_id
+                return known_users
+
+    def count_users(self):
+        """Метод для получения количества пользователей"""
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT COUNT(*) FROM users;")
+                return cursor.fetchone()[0]
+
+
+class UserWords:
+    """
+    Класс User_Words предназначен для управления связями
+    между пользователями и словами в базе данных, включая создание таблицы user_words,
+    добавление и удаление слов для конкретного пользователя,
+    а также получение слов, сохраненных пользователем.
+    """
+
+    def __init__(self, user_db, password_db, name_db):
+        self.user = user_db
+        self.password = password_db
+        self.db_name = name_db
+
+    def connect_to_database(self):
+        """Метод для создания подключения к базе данных"""
+        return psycopg2.connect(database=self.db_name, user=self.user, password=self.password)
+
+    def create_table_user_words(self):
+        """
+        Метод для создания таблицы user_words, если она еще не существует,
+        с полями user_id и word_id, связанными с таблицами users и words.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS user_words (
+                user_id INTEGER REFERENCES users(id),
+                word_id INTEGER REFERENCES words(id),
+                PRIMARY KEY (user_id, word_id)
+                    );''')
+
+    def add_word(self, user_id, word_id):
+        """
+        Метод для добавления нового слова в список сохраненных слов пользователя,
+        передавая user_id и word_id.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("INSERT INTO user_words (user_id, word_id) VALUES (%s, %s);",
+                               (user_id, word_id))
+                conn.commit()
+
+    def delete_word(self, user_id, word_id):
+        """
+        Метод для удаления слова из списка сохраненных слов пользователя,
+        передавая user_id и word_id.
+        """
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("DELETE FROM user_words WHERE user_id = %s AND word_id = %s;",
+                           (user_id, word_id))
+                conn.commit()
+
+    def get_user_words(self, user_id):
+        """
+        Метод для получения случайного слова из списка сохраненных слов пользователя,
+        его перевода и списка других случайных слов для создания карточек.
+        Возвращает target_word, translate_word, others и target_id.
+        Если у пользователя нет сохраненных слов, возвращает None.
+        """
+        others = []
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT word_id FROM user_words WHERE user_id = %s;", (user_id,))
+                word_ids = cursor.fetchall()
+                word_ids = [row[0] for row in word_ids]
+
+                if not word_ids:
+                    return None, None, None, None
+
+                cursor.execute('''SELECT MAX(id) FROM words''')
+                max_id = cursor.fetchone()[0]
+                target_id = random.choice(word_ids)
+                cursor.execute('''
+                                    SELECT en FROM words WHERE id = %s
+                                     ''', (target_id,))
+                target_word = cursor.fetchone()[0]
+                cursor.execute('''
+                                    SELECT ru FROM words WHERE id = %s
+                                     ''', (target_id,))
+                translate_word = cursor.fetchone()[0]
+                for _ in range(3):
+                    random_other_id = random.randint(0, max_id)
+                    if target_id != random_other_id:
+                        cursor.execute('''
+                                            SELECT en FROM words WHERE id = %s
+                                            ''', (random_other_id,))
+                        other_word = cursor.fetchone()[0]
+                        others.append(other_word)
+        return target_word, translate_word, others, target_id
+
+    def count_user_words(self, user_id):
+        """Метод для получения количества слов конкретного пользователя"""
+        with self.connect_to_database() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT COUNT(*) FROM user_words WHERE user_id = %s;", (user_id,))
+                return cursor.fetchone()[0]
+
+
+# чтение данных для подключения к БД
+user, password, db_name = read_user_data()
+# создаем экземпляр класса Words
+word = Words(user, password, db_name)
+# создаем таблицу words
+word.create_table_words()
+    # word.drop_table_words('user_words')
+    # word.drop_table_words('users')
+    # word.drop_table_words('words')
+# заполняем таблицу words
+word.fill_table_words()
+# создаем экземпляр класса Users
+users = Users(user, password, db_name)
+# создаем таблицу users
+users.create_table_users()
+# создаем экземпляр класса User_Words
+user_words = UserWords(user, password, db_name)
+# создаем таблицу user_words
+user_words.create_table_user_words()
+
+
---
+++ main.py
@@ -0,0 +1,218 @@
+"""
+Программа представляет собой телеграм-бота,
+предназначенного для изучения английских слов.
+Бот позволяет добавлять, удалять и учить слова в двух режимах:
+учить новые слова или учить сохраненные слова.
+"""
+
+import random
+import configparser
+from telebot import types, TeleBot, custom_filters
+from telebot.storage import StateMemoryStorage
+from telebot.handler_backends import State, StatesGroup
+from connect_with_db import word, users, user_words
+
+
+def read_token_telegram():
+    """
+    Функция считывает токен для доступа к Telegram API
+    из конфигурационного файла settings.ini.
+    """
+    config = configparser.ConfigParser()
+    config.read("/home/mladinsky/Study/settings.ini")
+    token_tg = config['TG']['TELEGRAM_TOKEN']
+    return token_tg
+
+
+print('Start telegram bot...')
+state_storage = StateMemoryStorage()
+token_bot = read_token_telegram()
+bot = TeleBot(token_bot, state_storage=state_storage)
+known_users = users.get_known_users()
+buttons = []
+
+
+def show_hint(*lines):
+    """ объединяет строки в одну строку с переносами для отображения подсказок"""
+    return '\n'.join(lines)
+
+
+def show_target(data):
+    """ формирует строку для отображения целевого слова и его перевода"""
+    return f"{data['target_word']} -> {data['translate_word']}"
+
+
+class Command:
+    """
+    Класс содержит названия команд для добавления,
+     удаления слов, выбора режимов обучения и перехода между ними.
+    """
+    ADD_WORD = 'Добавить слово ➕'
+    DELETE_WORD = 'Удалить слово🔙'
+    NEXT = 'Дальше ⏭'
+    LEARN_NEW = 'Учить новые слова'
+    LEARN_SAVED = 'Учить сохраненные слова'
+    BACK_TO_MODE = 'Назад к выбору режима'
+
+
+class MyStates(StatesGroup):
+    """
+    Класс определяет состояния для управления процессом
+    ввода и обработки данных пользователем
+    """
+    target_word = State()
+    translate_word = State()
+    another_words = State()
+    target_id = State()
+    learning_mode = State()
+
+
+@bot.message_handler(commands=['cards', 'start'])
+def create_cards(message):
+    """
+    Обработчик команд /cards и /start, который проверяет, является ли пользователь новым,
+    сохраняет его в базе данных, и предлагает выбрать режим обучения.
+    """
+    cid = message.chat.id
+    if cid not in known_users:
+        user_name = (bot.get_chat(cid)).first_name
+        bot.send_message(cid, f"Hello, {user_name}, let's study English...?")
+        users.save_user(user_name, cid)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        if 'learning_mode' not in data:
+            select_learning_mode(message)
+        else:
+            show_cards(message)
+
+
+def select_learning_mode(message):
+    """
+    Предлагает пользователю выбрать режим обучения
+    (новые или сохраненные слова) и устанавливает состояние learning_mode.
+    """
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    learn_new_btn = types.KeyboardButton(Command.LEARN_NEW)
+    learn_saved_btn = types.KeyboardButton(Command.LEARN_SAVED)
+    markup.add(learn_new_btn, learn_saved_btn)
+    bot.send_message(message.chat.id, "Выберите режим обучения:", reply_markup=markup)
+    bot.set_state(message.from_user.id, MyStates.learning_mode, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: message.text in [Command.LEARN_NEW, Command.LEARN_SAVED])
+def set_learning_mode(message):
+    """ Обработчик для установки выбранного режима обучения и вызова функции show_cards(message)."""
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['learning_mode'] = message.text
+    show_cards(message)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.NEXT)
+def show_cards(message):
+    """
+    Функция, которая загружает слово в зависимости от выбранного режима обучения
+     (новые или сохраненные слова) и отображает его вместе с вариантами перевода.
+    """
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        learning_mode = data.get('learning_mode')
+        if learning_mode == Command.LEARN_NEW:
+            target_word, translate_word, others, target_id = word.get_word_from_words()
+        elif learning_mode == Command.LEARN_SAVED:
+            cid = message.chat.id
+            uid = known_users[cid]
+            target_word, translate_word, others, target_id = user_words.get_user_words(uid)
+        if not target_word:
+            bot.send_message(message.chat.id,
+                "У вас нет сохраненных слов. Выберите режим - Учить новые слова")
+            select_learning_mode(message)
+            return
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    global buttons
+    buttons = []
+    target_word_btn = types.KeyboardButton(target_word)
+    buttons.append(target_word_btn)
+    other_words_btns = [types.KeyboardButton(word) for word in others]
+    buttons.extend(other_words_btns)
+    random.shuffle(buttons)
+    next_btn = types.KeyboardButton(Command.NEXT)
+    add_word_btn = types.KeyboardButton(Command.ADD_WORD)
+    delete_word_btn = types.KeyboardButton(Command.DELETE_WORD)
+    back_to_mode_btn = types.KeyboardButton(Command.BACK_TO_MODE)
+    buttons.extend([next_btn, add_word_btn, delete_word_btn, back_to_mode_btn])
+    markup.add(*buttons)
+    greeting = f"Выбери перевод слова:\n🇷🇺 {translate_word}"
+    bot.send_message(message.chat.id, greeting, reply_markup=markup)
+    bot.set_state(message.from_user.id, MyStates.target_word, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['target_word'] = target_word
+        data['translate_word'] = translate_word
+        data['other_words'] = others
+        data['target_id'] = target_id
+
+
+@bot.message_handler(func=lambda message: message.text == Command.BACK_TO_MODE)
+def back_to_mode(message):
+    """
+    Функция, которая удаляет текущий режим из данных
+    и вызывает функцию select_learning_mode для повторного выбора режима.
+    """
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        if 'learning_mode' in data:
+            del data['learning_mode']
+    select_learning_mode(message)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.NEXT)
+def next_cards(message):
+    """Функция, которая вызывает create_cards для отображения следующей карточки."""
+    create_cards(message)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.DELETE_WORD)
+def delete_word(message):
+    """ Функция, которая получает идентификатор слова и удаляет его из базы данных пользователя."""
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        cid = message.chat.id
+        uid = known_users[cid]
+        target_id = data['target_id']
+        user_words.delete_word(uid, target_id)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.ADD_WORD)
+def add_word(message):
+    """Функция, которая получает идентификатор слова и добавляет его в базу данных пользователя."""
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        cid = message.chat.id
+        uid = known_users[cid]
+        target_id = data['target_id']
+        user_words.add_word(uid, target_id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def message_reply(message):
+    """
+    Функция, которая проверяет ответ пользователя,
+    отображает сообщение с результатом (правильно или неправильно)
+    и обновляет клавиатуру с кнопками.
+    """
+    text = message.text
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        target_word = data['target_word']
+        if text == target_word:
+            hint = show_target(data)
+            hint_text = ["Отлично!❤", hint]
+            hint = show_hint(*hint_text)
+        else:
+            for btn in buttons:
+                if btn.text == text:
+                    btn.text = text + '❌'
+                    break
+            hint = show_hint("Допущена ошибка!",
+                             f"Попробуй ещё раз вспомнить слово 🇷🇺{data['translate_word']}")
+    markup.add(*buttons)
+    bot.send_message(message.chat.id, hint, reply_markup=markup)
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+
+bot.infinity_polling(skip_pending=True)
